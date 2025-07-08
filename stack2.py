b37d062d9527fcc812d0fdf2e1792a8894323e24
alulacoder <rasalula2024@gmail.com>
2025-07-08 19:56:55 +0100

    Add files via upload

---
+++ stack2.py
@@ -0,0 +1,9 @@
+stack = ["Alula", "Danstan", "smart", "people", "Earth"]
+top= stack[-1]
+print(top)
+size = len(stack)
+print(size)
+# pop = stack.remove(-1)
+del stack[-1]
+# print(pop)
+print(stack)
